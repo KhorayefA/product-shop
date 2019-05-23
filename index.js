96825edefd1feb196d0b7959d46cdd4ac139f4ad
Abdulellah Alkhorayef <38599353+KhorayefA@users.noreply.github.com>
2019-05-23 11:07:14 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
